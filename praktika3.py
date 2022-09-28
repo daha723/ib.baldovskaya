4cb8d44541b8429208f9ccf082f3f44309b2fd32
daha723 <113470838+daha723@users.noreply.github.com>
2022-09-28 22:10:41 +0300

    Add files via upload

---
+++ praktika3.py
@@ -0,0 +1,13 @@
+#13
+a = int (input('Введите число a: '))
+b = int (input('Введите число b: '))
+x = int
+if a<b:
+    x=(2*a)+(2*b)
+    print(x)
+elif a>b:
+    x=a-b+1
+    print(x)
+elif a==b:
+    x=b**2-b
+    print(x)
